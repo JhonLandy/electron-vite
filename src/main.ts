f79984051902c1aab87be52ed3a1662b491031b6
huaweiserver <709364178@qq.com>
2023-01-06 15:52:57 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import App from "@src/App.vue";
+import "@src/css/index.sass";
 import "tailwindcss/tailwind.css";
 import { createApp } from "vue";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import "vuetify/styles";
-import "./css/index.sass";
 const vuetify = createVuetify({
     components,
     directives,
